07b72fa081d1d3f9a8cae2bc1bfbe5dcdb3e3493
ayushbhargava22 <57057106+ayushbhargava22@users.noreply.github.com>
2020-04-05 16:18:21 +0530

    Add files via upload

---
+++ notification.js
@@ -0,0 +1,13 @@
+
+document.getElementById('bell').addEventListener("click", function(){
+    var box = document.getElementById("not");
+        if(box.style.display == "none")
+        {
+            box.style.display = "block"
+        }
+        else
+        {
+            box.style.display = "none"
+        }
+});
+
---
+++ setting_drop.js
@@ -0,0 +1,11 @@
+document.getElementById('cog').addEventListener("click", function(){
+    var box = document.getElementById("set");
+        if(box.style.display == "none")
+        {
+            box.style.display = "block"
+        }
+        else
+        {
+            box.style.display = "none"
+        }
+});
